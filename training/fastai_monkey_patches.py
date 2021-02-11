0c5286277f0a81efaa7ee563f26134f44a0f4caa
Shravan <ludovic.amruthalingam@unibas.ch>
2021-02-11 09:19:30 +0100

    bugfix fastai loss

--- training/fastai_monkey_patches.py
+++ training/fastai_monkey_patches.py
@@ -2,14 +2,14 @@ import torch
 import fastai.vision.all as fv
 
 
-# OFFICIAL VERSION DOES NOT SEEM TO SUPPORT THE WEIGHT ARGS
+# OFFICIAL VERSION DOES NOT SEEM TO SUPPORT THE WEIGHT ARGS + does not use provided axis
 class FixedLabelSmoothingCrossEntropyFlat(fv.BaseLoss):
     "Same as `LabelSmoothingCrossEntropy`, but flattens input and target."
     y_int = True
     @fv.use_kwargs_dict(keep=True, weight=None, eps=0.1, reduction='mean')
     def __init__(self, *args, axis=-1, **kwargs): super().__init__(FixedLabelSmoothingCrossEntropy, *args, axis=axis, **kwargs)
-    def activation(self, out): return fv.F.softmax(out, dim=-1)
-    def decodes(self, out):    return out.argmax(dim=-1)
+    def activation(self, out): return fv.F.softmax(out, dim=self.axis)
+    def decodes(self, out):    return out.argmax(dim=self.axis)
 
 
 # EXACT COPY, NEEDED OTHERWISE FIXED FLAT COMPLAINS OVER WEIGHT ARGS
