37d080a48d8353960a5212dbfb5d31fc01e72c9a
Shravan <ludovic.amruthalingam@unibas.ch>
2021-04-08 11:58:08 +0200

    sharpness score

--- general/common.py
+++ general/common.py
@@ -328,6 +328,24 @@ def quick_img_size(img_path):
     return height, width
 
 
+def im_sharpness_score(im):
+    """ im is a numpy array of rgb image
+    Returns sharpness score (float): the lower the score the blurrier the image,
+    the higher the score, the sharper the image
+
+    https://www.pyimagesearch.com/2015/09/07/blur-detection-with-opencv/
+    Laplacian is used to measure the 2nd derivative of an image,
+    it highlights regions of an image containing rapid intensity changes (edges)
+
+    Assumption is that if an image contains high variance then there is a wide spread of responses,
+    both edge-like and non-edge like, representative of a normal, in-focus image.
+    But if there is very low variance, then there is a tiny spread of responses,
+    indicating there are very little edges in the image.
+    As we know, the more an image is blurred, the less edges there are.
+    """
+    return cv2.Laplacian(cv2.cvtColor(im, cv2.COLOR_RGB2GRAY), cv2.CV_64F).var()
+
+
 def img_bgr_to_rgb(im):
     if len(im.shape) != 3:
         raise Exception(f"Error cannot convert from bgr to rgb, im shape is {im.shape}")
