4ccd410d2b46961790b159eb2293e7f1066fd1f2
Shravan <ludovic.amruthalingam@unibas.ch>
2022-04-27 15:13:43 +0200

    refactor split dataset

--- general/split_dataset.py
+++ general/split_dataset.py
@@ -12,109 +12,198 @@ __copyright__ = (
 )
 
 import os
-import json
 import random
 import argparse
-import numpy as np
-from shutil import copy
-from sklearn.model_selection import train_test_split
+from functools import partial
+
+from tqdm import tqdm
+from p_tqdm import p_umap
 
 import common
+import common_img as cimg
 
 
-def dataset_split(args, directory):
-    """Splits directory files in train/test
-    :param args: command line arguments
-    :param directory: str, directory to be splitted
-    :return: dict with str train and test keys and values the corresponding files
+def knapSack(W, wt, val):
+    """Solves knapSack problem, source https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
+    :param W: total capacity to fill
+    :param wt: weights of items
+    :param val: values of items
+    :return total value, dynamic programming table
     """
-    files = common.list_files(os.path.join(args.data, directory), full_path=True)
-    train, test = train_test_split(files, test_size=args.test_size, random_state=args.seed, shuffle=True)
-    return {'train': train, 'test': test}
+    n = len(wt)
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0: K[i][w] = 0
+            elif wt[i-1] <= w: K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w])
+            else: K[i][w] = K[i-1][w]
+    return K[n][W], K
 
 
-def copy_imgs(args, cat, splits):
-    """Copies images from the different splits
-    :param args: command line arguments
-    :param cat: str, directory name to copy image from
-    :param splits: dict with train and test files
+def knapSack_with_items(W, wt, val, items):
+    """Solves knapSack problem, source https://www.geeksforgeeks.org/printing-items-01-knapsack/
+    :param W: total capacity to fill
+    :param wt: weights of items
+    :param val: values of items
+    :return total value and selected items
     """
-    print(f"Copying {cat} images ...")
-    for ds, files in splits.items():
-        dest_dir = os.path.join(args.dest, ds, cat)
-        common.maybe_create(dest_dir)
-        for file in files:
-            filename, ext = os.path.splitext(os.path.basename(file))
-            copy(file, os.path.join(dest_dir, filename + ext))
-            for ld in args.label_dirs:
-                lf = filename + args.lext
-                lab_dest_dir = common.maybe_create(dest_dir.replace(cat, ld))
-                copy(os.path.join(args.data, ld, lf), os.path.join(lab_dest_dir, lf))
+    n, selected_items = len(wt), []
+    res, K = knapSack(W, wt, val)
+    w = W
+    for i in range(n, 0, -1):
+        if res <= 0: break
+        # either the result comes from the
+        # top (K[i-1][w]) or from (val[i-1]
+        # + K[i-1] [w-wt[i-1]]) as in Knapsack
+        # table. If it comes from the latter
+        # one/ it means the item is included.
+        if res == K[i - 1][w]: continue
+        else:
+            # This item is included.
+            selected_items.append(items[i - 1])
+            # Since this weight is included
+            # its value is deducted
+            res = res - val[i - 1]
+            w = w - wt[i - 1]
+    return res, selected_items
 
 
-def split_coco_labels(coco_json, files):
-    """Creates coco json file for provided files
-    :param coco_json: str, original coco json file
-    :param files: list, image filepaths
-    :return: dict, new coco json file
+def test_knapSack():
+    """Simple example of the knapSack problem"""
+    items = ['a','b','c']
+    val = [ 21, 22, 29 ]
+    wt = [ 21, 22, 29 ]
+    knapSack_with_items(51, wt, val, items)
+
+
+def split_files(seed, dirpath, test_prop, get_patient_id):
+    """Split images from directory into train and test images
+    :param seed: random use for split
+    :param dirpath: directory path
+    :param test_prop: test proportion
+    :param get_patient_id: function to extract patient id from image path
+    :return tuple of lists with train and test images
     """
-    with open(coco_json, 'r') as f:
-        labels = json.load(f)
-        files = [os.path.basename(f) for f in files]
-        labels['images'] = [l for l in labels['images'] if os.path.basename(l['file_name']) in files]
-        image_ids = [i['id'] for i in labels['images']]
-        labels['annotations'] = [l for l in labels['annotations'] if l['image_id'] in image_ids]
-        return labels
+    impaths = common.list_images(dirpath, full_path=True)
+    patient_pics = {}
+    for impath in impaths:
+        pid = get_patient_id(impath)
+        if pid in patient_pics:
+            patient_pics[pid].append(impath)
+        else:
+            patient_pics[pid] = [impath]
+    pids = list(patient_pics.keys())
+    random.Random(seed).shuffle(pids)  # added for HE dataset (4th april 22)
+    pid_npics = [len(patient_pics[pid]) for pid in pids]
+
+    # better sample train size first in case lots of pictures with same pid
+    train_size = len(impaths) - int(test_prop * len(impaths)) - 1
+    _, train_pids = knapSack_with_items(train_size, pid_npics, pid_npics, pids)
+    test_pids = [pid for pid in pids if pid not in train_pids]
+    train_impaths = [impath for pid in train_pids for impath in patient_pics[pid]]
+    test_impaths = [impath for pid in test_pids for impath in patient_pics[pid]]
+    return train_impaths, test_impaths
+
+
+def split_and_save(category, dirpath, test_prop, dest_root, pidx_fn, seed):
+    """Splits directory and save images in respective dir
+    :param category: directory name
+    :param dirpath: directory path
+    :param test_prop: test proportion
+    :param dest_root: destination directory path
+    :param pidx_fn: function to extract patient id from image path
+    :param seed: random seed for split
+    """
+    train_impaths, test_impaths = split_files(seed, dirpath, test_prop, pidx_fn)
+    for setname, impaths in zip(('train', 'test'), (train_impaths, test_impaths)):
+        dest_dir = common.maybe_create(dest_root, setname, category)
+        for impath in impaths:
+            # shutil copy can create issues...
+            cimg.save_img(cimg.load_img(impath), os.path.join(dest_dir, os.path.basename(impath)))
+
+
+
+
+def split_classif_dataset(root_dir, test_prop, pidx_fn, seed):
+    """Splits typical classification dataset
+    :param root_dir: image directory path
+    :param test_prop: test proportion
+    :param pidx_fn: function to extract patient id from image path
+    :return:
+    """
+    classes = common.list_dirs(root_dir)
+    dest_root = common.maybe_create(root_dir.rstrip("/") + "_splitted")
+    fn = partial(split_and_save, test_prop=test_prop, dest_root=dest_root, pidx_fn=pidx_fn, seed=seed)
+    p_umap(fn, classes, [os.path.join(root_dir, c) for c in classes])
+
+
+
+
+def split_segm_dataset(root_dir, test_prop, pidx_fn, seed, imdir='images', mdir='masks'):
+    """Splits typical segmentation dataset
+    :param root_dir: directory path where images and masks are located
+    :param test_prop: test proportion
+    :param pidx_fn: function to extract patient id from image path
+    :param imdir: image directory name
+    :param mdir: mask directory name
+    """
+    dest_root = common.maybe_create(root_dir.rstrip("/") + "_splitted")
+    split_and_save(imdir, os.path.join(root_dir, imdir), test_prop, dest_root, pidx_fn, seed)
+    for d in ['train', 'test']:
+        dest_mdir = common.maybe_create(dest_root, d, mdir)
+        for imname in tqdm(common.list_images(os.path.join(dest_root, d, imdir))):
+            mpath = os.path.join(root_dir, mdir, os.path.splitext(imname)[0] + '.png')
+            cimg.save_img(cimg.load_img(mpath), os.path.join(dest_mdir, os.path.basename(mpath)))
+
+
+def usz_get_patient_id(impath):
+    """Extract patient id from usz image path
+    :param impath: image path
+    :return: patient id as str
+    """
+    imname = os.path.basename(impath)
+    #return imname.split('_')[0] if imname[0].isnumeric() else imname.split('-')[0]
+    return imname.split('_')[0]
+
+
+def he_get_patient_idx(impath):
+    """Extract patient id from hand eczema image path
+    :param impath: image path
+    :return: patient id as str
+    """
+    idx = os.path.basename(impath).split('_')[0]
+    # if photobox img then idx is the patient idx
+    # else last 5 char are D (dorsal) or P (palm) + ext
+    return idx[:-5] if '.jpg' in idx else idx
 
 
 def main(args):
     """Splits dataset following command line arguments
     :param args: command line arguments
     """
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-
-    dirs = [d for d in common.list_dirs(args.data) if d not in args.label_dirs and d not in args.ignore]
-    if args.coco_json:
-        # with coco format, images are in single dir
-        splits = dataset_split(args, dirs[0])
-        json_dir, json_file = args.label_dirs[0], os.path.basename(args.coco_json)
-        # remove json dir since from args.label_dirs since no images are there to copy
-        args.label_dirs = []
-        copy_imgs(args, dirs[0], splits)
-        for ds, files in splits.items():
-            split_labels = split_coco_labels(args.coco_json, files)
-            json_path = os.path.join(json_dir, json_file.replace('.json', f'_{ds}.json'))
-            json.dump(split_labels, open(json_path, 'w'), sort_keys=True, indent=4, separators=(',', ': '))
-    else:
-        for d in dirs:
-            copy_imgs(args, d, dataset_split(args, d))
+    if args.pidx_fn == 'usz':
+        pidx_fn = usz_get_patient_id
+    elif args.pidx_fn == 'he':
+        pidx_fn = he_get_patient_idx
+    if args.classif:
+        split_classif_dataset(args.data, args.test_size, pidx_fn, args.seed)
+    elif args.segm:
+        split_segm_dataset(args.data, args.test_size, pidx_fn, args.seed)
     print("done")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Split dataset in train/test dataset")
     parser.add_argument('--data', type=str, required=True, help="source dataset root directory")
-    parser.add_argument('--dest', type=str, help="directory where the slits should be saved")
     parser.add_argument('--test-size', default=.2, type=float, help="Proportion of test set")
     parser.add_argument('--seed', default=42, type=int, help="random seed")
-    parser.add_argument('--label-dirs', nargs='+', default=[], type=str, help="dirs with labels")
-    parser.add_argument('--ignore', nargs='+', default=[], type=str, help="ignore dirs")
-    parser.add_argument('--lext', type=str, default='.png', help="label file extension")
-    parser.add_argument('--coco-json', type=str, help="if given will not look for masks")
+    parser.add_argument('--pidx-fn', type=str, default="usz", help="Function to extract patient idx")
+    parser.add_argument('--classif', action='store_true', help="Classification dataset")
+    parser.add_argument('--segm', action='store_true', help="Segmentation dataset")
     args = parser.parse_args()
 
     common.check_dir_valid(args.data)
     args.data = args.data.rstrip('/')
-
-    if args.dest is None:
-        args.dest = common.maybe_create(f'{args.data}_splitted')
-    else:
-        common.check_dir_valid(args.dest)
-        args.dest = args.dest.rstrip('/')
-
-    if args.coco_json is not None:
-        common.check_file_valid(args.coco_json)
-        args.label_dirs = [os.path.basename(os.path.dirname(args.coco_json))]
 
     common.time_method(main, args)
