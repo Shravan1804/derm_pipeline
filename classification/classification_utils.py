a93e5b9067c43804470f065108ed25847cf1da60
Fabian Gröger <fabian.groeger@bluewin.ch>
2022-05-09 10:45:28 +0200

    added new classification head with correctly initalized weights

--- classification/classification_utils.py
+++ classification/classification_utils.py
@@ -55,3 +55,17 @@ def conf_mat(targs, preds, cats, normalize=True, epsilon=1e-8):
     return cm
 
 
+class LinearClassifier(torch.nn.Module):
+    """Linear layer as classification head."""
+    def __init__(self, dim, num_labels=1000):
+        super(LinearClassifier, self).__init__()
+        self.num_labels = num_labels
+        self.linear = torch.nn.Linear(dim, num_labels)
+        self.linear.weight.data.normal_(mean=0.0, std=0.01)
+        self.linear.bias.data.zero_()
+
+    def forward(self, x):
+        # flatten
+        x = x.view(x.size(0), -1)
+        # linear layer
+        return self.linear(x)
